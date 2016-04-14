56d2dcd1f75d4e662d2f2c2d20fb2a6e6eaf3139
Rashedul Islam <ririyad.291@gmail.com>
2016-04-14 09:12:01 +0600

    upto 14

--- src/posts/views.py
+++ src/posts/views.py
@@ -5,5 +5,17 @@ from django.shortcuts import render
 # Create your views here.
 # Here I am creating function-based views
 
-def post_home(request):
-    return HttpResponse("<h2>Hello!</h2>")
+def post_create(request):
+    return HttpResponse("<h2>Create</h2>")
+
+def post_detail(request): # retrieve
+    return HttpResponse("<h2>Detail</h2>")
+
+def post_list(request):
+    return render(request, "index.html", {})
+
+def post_update(request):
+    return HttpResponse("<h2>Update</h2>")
+
+def post_delete(request):
+    return HttpResponse("<h2>Delete</h2>")
