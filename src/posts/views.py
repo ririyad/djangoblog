afad4cddcca2ca3ba37da84bbcaa909c0c180b55
Rashedul Islam <ririyad.291@gmail.com>
2016-04-18 11:09:34 +0600

    corrected form session 28

--- src/posts/views.py
+++ src/posts/views.py
@@ -67,7 +67,7 @@ def post_update(request, id=None):
     if not request.user.is_staff or not request.user.is_superuser:
         raise Http404
     instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, request.FILES or None)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
