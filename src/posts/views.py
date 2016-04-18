49d60ef581b8a3d326e16872ac36640d5b147700
Rashedul Islam <ririyad.291@gmail.com>
2016-04-18 09:06:04 +0600

    upto 30 (ignored 29)

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,4 @@
+from urllib import  quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
@@ -29,9 +30,11 @@ def post_create(request):
 def post_detail(request, id=None): # retrieve
     # instance = Post.objects.get(id=1)
     instance = get_object_or_404(Post, id=id)
+    share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
         "instance": instance,
+        "share_string": share_string,
     }
     return render(request, "post_detail.html", context)
 
